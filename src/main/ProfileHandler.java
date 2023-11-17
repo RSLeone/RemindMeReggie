e04e80ebf741d4faa98a26b603fd54d2e1211584
RSLeone <ryansleone@hotmail.com>
2023-11-17 14:42:54 -0600

    Implemented all ProfileHandler methods except deleteCurrentProfile

--- src/main/ProfileHandler.java
+++ src/main/ProfileHandler.java
@@ -3,106 +3,221 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import main.Persistence;
+import main.PersistenceFactory;
+import main.Profile;
 import main.PersistenceFactory.persistenceType;
 
 public class ProfileHandler {
     
     private static Profile currentProfile;
 
-    //Revisit this after Andrew finishes presistence
+    /**
+     * Attempts to login with the entered username and password. If successful, sets the currentProfile to the 
+     *      Profile with the entered uesrname
+     * @param username - the username of the profile to log in to
+     * @param password - the password of the profile to log in to
+     * @return 0 if a successful log in occured, otherwise a negative integer that corresponds to what went wrong
+     */
     public static int login(String username, String password)
     {
-        //Search use persistence.load() to get the profile witht the passed in username and make sure the password is valid
+
+        //If the username is invalid return code -101
+        if(username.length() <= 0)
+        {
+            return -101;
+        }
+
+        //If the password is invalid return code -102
+        if(password.length() <= 0)
+        {
+            return -102;
+        }
+
+        //Use persistence.load() to get the profile with the passed in username
         PersistenceFactory pf = new PersistenceFactory();
-        pf.getPersistent(persistenceType.LocalFile)
-        Profile loadedProfile = persistence.load();
+        Persistence persistence = pf.getPersistent(persistenceType.JsonFile);
+        Profile loadedProfile = persistence.load(username);
+
+        //If the loadedProfile is null beacuse no Profile with the username passed in exists, return code -104
+        if(loadedProfile == null)
+        {
+            return -104;
+        }
+        
+        //Hashing the entered password in isSamePassowrd may throw an exception
         try
         {
-            if(same password entered) 
+            if(isSamePassword(loadedProfile, password)) 
             {
                 currentProfile = loadedProfile; 
             }
+            //If the entered password does not match the password for the username, return code -104
             else
             {
-
+                return -104;
             }
         }
-        catch
+        //isSamePassword failed to hash the entered password so return code -103
+        catch(NoSuchAlgorithmException e)
         {
-            checking that the passwords are the same may throw an exception 
+            return -103;
         }
-
-        */
  
+        //Successful login so return code 0
         return 0;
 
     }
 
-    //Revisit this after Andrew finished presistence
+    /**
+     * Attempts to save the current profile and log out the current profile
+     * @return true is the current profile could be saved and was logged out sucessfully, otherwise returns false
+     */
     public static boolean logOut()
     {
-        //Save the profile useing persistence.save()
-        currentProfile = null;
+        
+        PersistenceFactory pf = new PersistenceFactory();
+        Persistence persistence = pf.getPersistent(persistenceType.JsonFile);
 
-        return true;
+        //If the currentProfile could be saved sucessfully, set the currentProfile to null and return true
+        if(persistence.save(currentProfile))
+        {
+            currentProfile = null;
+            //The currentProfile was successfully logged out
+            return true;
+        }
+
+        //The currentProfile could not be saved, so it could not be logged out
+        return false;
+
     }
 
-    //Revisit this after Andrew finished presistence
+    /**
+     * Attempts to create a new profile and add it to the system
+     * @param username - the username of the profile to be created
+     * @param unhashedPassword - the password of the profile to be created
+     * @return 0 if a successful profile creation occured, otherwise a negative integer that corresponds to what went wrong
+     */
     public static int createNewProfile(String username, String unhashedPassword)
     {
+
+        //If the username is invalid return code -101
+        if(username.length() <= 0)
+        {
+            return -101;
+        }
+        //If the password is invalid return code -102
+        if(unhashedPassword.length() < 8)
+        {
+            return -102;
+        }
+
+        //Hashing the entered password in hashPassword may throw an exception
         try 
         {
             String hashedPassword = hashPassword(unhashedPassword);
             Profile newProfile = new Profile(username, hashedPassword);
-            //Save the profile useing persistence.save()
+            
+            PersistenceFactory pf = new PersistenceFactory();
+            Persistence persistence = pf.getPersistent(persistenceType.JsonFile);
+
+            //If the system fails to save the new profile, return code -105
+            if(!persistence.save(newProfile))
+            {
+                return -105;
+            }
+
         }
+        //hashPassword failed to hash the entered password so return code -103
         catch(NoSuchAlgorithmException e)
         {
-            System.out.println("The password entered could not be set. Please try creating a profile again.");
+            return -103;
         }
 
-        return true;
+        //The new profile was created and saved sucessfully so return code 0
+        return 0;
     }
 
-    //Revisit
+    /**
+     * Attempts to update the currentProfile's username and save the profile
+     * @param newUserName - the username that the currentProfile's username should be updated to
+     * @return 0 if the currentProfile's username was updated and was saved successfully, otherwise a negative integer that corresponds to what went wrong
+     */
     public static int editCurrentProfileUsername(String newUserName)
     {
-        //Save the profile using persistence.save()
 
-        return true;
+        //Need to save the previous username in case the save fails
+        String previousUsername = currentProfile.getUsername();
+
+        //If the newUsername is invalid return code -101
+        if(newUserName.length() <= 0)
+        {
+            return -101;
+        }
+
+        //Update the currentProfile's username
+        currentProfile.setUsername(newUserName);
+
+        PersistenceFactory pf = new PersistenceFactory();
+        Persistence persistence = pf.getPersistent(persistenceType.JsonFile);
+
+        //If the system fails to save the new profile, revert the currentProfile's username and return code -105
+        if(!persistence.save(currentProfile))
+        {
+            currentProfile.setUsername(previousUsername);
+            return -105;
+        }
+
+        //The system update the currentProfile's username and saved the currentProfile successfullly so return code 0
+        return 0;
     }
 
-    //Revisit
     /**
-    * If possible, hashes newPassword and sets the currentProfile's hashed password to the result and saves the profile, 
-    *       otherwise prints that the new password could not be set
-    * @param newPassword - the password to be hashed then set
-    */
+     * Attempts to edit the currentProfile's hashedPassword and save the currentProfile
+     * @param newPassword - the new password to be hashed and update the currentProfile
+     * @return 0 if the the currentProfile's hashedPassword was updated and the currentProfile was saved sucessfully, otherwise a negative integer that corresponds to what went wrong
+     */
     public static int editCurrentProfilePassword(String newPassword) 
     {
-        
+        //Hashing the entered password in hashPassword may throw an exception
         try
         {
+            //Need to store the previous hashedPassword in case the save fails
+            String previousHashedPassword = currentProfile.getHashedPassword();
+
+            //Update the currentProfile's hashedPassword
             currentProfile.setHashedPassword(hashPassword(newPassword));
-            //Save the profile using persistence.save()
+            
             PersistenceFactory pf = new PersistenceFactory();
-            pf.getPersistent(persistenceType.LocalFile);
+            Persistence persistence = pf.getPersistent(persistenceType.JsonFile);
+
+            //Failed to save the currentProfile so revert the hashedPassword of the currentProfile and return code -105
+            if(!persistence.save(currentProfile))
+            {
+                currentProfile.setHashedPassword(previousHashedPassword);
+                return -105;
+            }
         }
+        //hashPassword failed to hash the entered password so return code -103
         catch(NoSuchAlgorithmException e)
         {
-            System.out.println("The new password entered could not be set. Please try setting a new password again.");
-            return false;
+            return -103;
         }
         
-        return true;
+        //The currentProfile's hashedPassword was updated and the currentProfile was saved sucessfully so return code 0
+        return 0;
 
     }
 
     public static boolean deleteCurrentProfile()
     {
-
+        PersistenceFactory pf = new PersistenceFactory();
+        Persistence persistence = pf.getPersistent(persistenceType.JsonFile);
+        
+        return persistence.delete(currentProfile);
     }
 
+    //Getter for currentProfile
     public static Profile getCurrentProfile() {
         return currentProfile;
     }
