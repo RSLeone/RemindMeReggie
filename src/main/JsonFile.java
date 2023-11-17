f56189a6ac945a55e4a2084f1ebda96032d73cb4
AndrewVanHoveln <andrewvanhoveln2001@gmail.com>
2023-11-17 17:13:56 -0600

    Added delete functionality

--- src/main/JsonFile.java
+++ src/main/JsonFile.java
@@ -1,4 +1,5 @@
 package main;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -57,7 +58,14 @@ public class JsonFile extends Persistence{
     @Override
     public void setDefaultLocation() {
         setLocation("./profiles");
-    }   
+    }
+    
+    @Override
+    public boolean delete(String username){
+        File file = new File(getLocation() + "/" + username + ".json");
+
+        return file.delete();
+    }
 
     
 }
